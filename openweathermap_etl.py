107b5ea546761900ab3e8348025f551d56f2d2d2
Citlally Contreras <97648752+citlallyc@users.noreply.github.com>
2024-11-02 19:16:43 -0700

    Update transformation step in etl

--- openweathermap_etl.py
+++ openweathermap_etl.py
@@ -6,6 +6,7 @@ import logging
 from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from airflow import DAG
+import datetime
 from datetime import timedelta
 import pendulum
 from airflow.decorators import task
@@ -28,7 +29,7 @@ with DAG(
         "start_date": pendulum.datetime(2024, 10, 31, 2, tz="UTC"),
         "retries": 3,
         "retry_delay": timedelta(minutes=3)},
-    schedule_interval = "@once",
+    schedule_interval = "*/30 * * * *",
     description = "Open Weather Map Data ETL Pipeline"
  ) as dag:
 
@@ -39,6 +40,7 @@ with DAG(
         try:
             response = requests.get(url)
             response.raise_for_status()
+
             data = response.json()
             message = "API extraction successful"
             logging.info("Log: {}".format(message))
@@ -46,6 +48,11 @@ with DAG(
             return data
 
         except requests.exceptions.RequestException as e:
+            if response.status_code == 429:
+                message = 'WARNING: Rate limit reached!'
+                logger.error(message)
+                print(message)
+
             logging.error("Log: EXCEPTION | Error on API {}".format(e))
             print("Print: EXCEPTION | Error on API : {} ".format(e))
             raise
@@ -60,13 +67,13 @@ with DAG(
                 "country": data["sys"]["country"],
                 "city_id": data["id"],
                 "city_name": data["name"],
-                "weather": data["weather"][0]["description"],
-                "temperature": data["main"]["temp"],
+                "weather_description": data["weather"][0]["description"],
+                "temperature": (data["main"]["temp"] - 273.15) * 9/5 + 32,
                 "humidity": data["main"]["humidity"],
                 "visibility": data["visibility"],
                 "wind_speed": data["wind"]["speed"],
                 "cloud_percent": data["clouds"]["all"],
-                "point_in_time": data["dt"]
+                "point_in_time": (datetime.datetime.utcfromtimestamp(data["dt"]) + datetime.timedelta(seconds=data["timezone"])).strftime("%Y-%m-%d %H:%M:%S")#datetime.datetime.fromtimestamp(data["dt"]).strftime("%Y-%m-%d %H:%M:%S")
             }
 
             message = "Successfully transformed JSON API response into dictionary"
@@ -83,17 +90,31 @@ with DAG(
     @task()
     def load_weather_data(transformed_weather):
         try:
-            # Load transformed data into Pandas dataframe
+            # load transformed data into Pandas dataframe
             loading_weather_df = pd.DataFrame([transformed_weather])
 
-            # Create a connection to the PostgreSQL database
+            # create a connection to the PostgreSQL database
             engine = create_engine(db_connection)
             message = "Successfully connected to Postgres"
             logging.info("Log: {}".format(message))
             print("Print: {}".format(message))
 
-            # Load the DataFrame into the database
-            loading_weather_df.to_sql('hourly_weather_histories', con=engine, if_exists='append', index=False)
+            insert_query = """
+            INSERT INTO weather_data_histories (
+            latitude, longitude, country, city_id, city_name,
+            weather_description, temperature, humidity, visibility, wind_speed,
+            cloud_percent, point_in_time
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+
+            # load data into database
+            for index, row in loading_weather_df.iterrows():
+                engine.execute(insert_query, tuple(row))
+
+            message = "Successfully merged into Postgres"
+            logging.info("Log: {}".format(message))
+            print("Print: {}".format(message))
 
         except SQLAlchemyError as e:
             logger.error("Log: EXCEPTION | Error loading to Postgres {}".format(e))
